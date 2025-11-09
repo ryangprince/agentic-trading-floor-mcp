833f49df98aa8152760428331aec851935532b9f
ryangprince <rpprince18@gmail.com>
2025-11-08 21:01:09 -0500

    added database.py and market.py

---
+++ database.py
@@ -0,0 +1,36 @@
+import sqlite3
+import json
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+DB = "accounts.db"
+
+with sqlite3.connect(DB) as conn:
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY, account TEXT)')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS logs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            datetime DATETIME,
+            type TEXT,
+            message TEXT
+        )
+    '''
+    )
+    cursor.execute('CREATE TABLE IF NOT EXISTS market (date TEXT PRIMARY KEY, data TEXT)')
+    conn.commit()
+    
+def write_account(name, account_dict):
+    json_data = json.dumps(account_dict)
+    with sqlite3.connect(DB) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO accounts (name, account)
+            VALUES (?, ?)
+            ON CONFLICT(name) DO UPDATE SET account=excluded.account
+        ''', (name.lower(), json_data))
+        conn.commit()
+        
